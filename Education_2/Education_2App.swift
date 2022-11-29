b523521b1c1f30f5490698ce6842c5a3fe50d19a
Radyush_Maksim <profa4oka98@mail.ru>
2022-11-29 22:47:28 +0300

    Initial Commit

---
+++ Education_2/Education_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Education_2App.swift
+//  Education_2
+//
+//  Created by Максим Радюш on 29.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Education_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
